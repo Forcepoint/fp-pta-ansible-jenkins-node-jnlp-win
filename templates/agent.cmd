b2665493598a5e4ba82eb724590d7f8ae5aa9731
Jeremy Cornett <jeremy.cornett@forcepointgov.com>
2022-06-23 16:46:03 -0600

    Incase multiple versions of java are installed and on the path, use the java version that's pointed to by JAVA_HOME.

--- templates/agent.cmd
+++ templates/agent.cmd
@@ -1,4 +1,4 @@
-java -jar {{ jenkins_node_path }}\agent.jar -jnlpUrl {{ jenkins_node_master_url }}/computer/{{ jenkins_node_name }}/slave-agent.jnlp -secret JENKINS_NODE_SECRET -workDir "{{ jenkins_node_path }}"
+"%JAVA_HOME%\bin\java" -jar {{ jenkins_node_path }}\agent.jar -jnlpUrl {{ jenkins_node_master_url }}/computer/{{ jenkins_node_name }}/slave-agent.jnlp -secret JENKINS_NODE_SECRET -workDir "{{ jenkins_node_path }}"
 
 set agenExitCode=%errorlevel%
 
